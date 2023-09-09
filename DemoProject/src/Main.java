80b98670469b3d22687be47a35c758aaa5b28df3
nom1gop <120338223+nom1gop@users.noreply.github.com>
2023-09-10 01:36:47 +0300

    Update Main.java

--- DemoProject/src/Main.java
+++ DemoProject/src/Main.java
@@ -2,6 +2,6 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Date());
+        System.out.println("yesterday was " new Date());
     }
 }
