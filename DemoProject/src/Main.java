dfec1189f23605288ebaa10cfead17648c6b3d6c
Egor Shestakov <lepomsois@mail.ru>
2023-09-12 10:03:26 +0300

    code for another method added

--- DemoProject/src/Main.java
+++ DemoProject/src/Main.java
@@ -3,9 +3,10 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         System.out.println("today is yesterday");
+        anotherMethod();
     }
     public static void anotherMethod() {
-
+        System.out.println("text for another method");
     }
 
 }
